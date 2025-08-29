e3c2f99b8807e9766799b238484e57de82c8f62f
Asus <ma4301511@gmail.com>
2025-08-29 23:55:22 +0330

    طراحی صفحه اول

--- home_app/views.py
+++ home_app/views.py
@@ -3,12 +3,20 @@ from cantact_app.models import accuntmodel
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+profilestatus =['']
+
 def home_def(request):
     login_user = 'false'
     if request.user.is_authenticated:
         login_user = 'true'
+        us = accuntmodel.objects.all()
+        for u in us:
+            if u.melicode == request.user.username:
+                profilestatus[0] = f"{u.firstname} {u.lastname}  "
+
     return render(request,'new_home.html',context={
                                                                 'login_user':login_user,
+                                                                 'profilestatus':profilestatus[0],
                                                                 })
 
 def logute(request):
