1e967ceed612d1fc2d558374e9368a58bcb513f8
Asus <ma4301511@gmail.com>
2025-10-19 09:09:20 +0330

    ا

--- plasco/settings.py
+++ plasco/settings.py
@@ -164,14 +164,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT='/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-# این مسیر رو عوض کن به یه جای خارج از پوشه پروژه
-MEDIA_ROOT = '/home/username/public_html/media_files/'  # یا هر مسیر دیگه
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
